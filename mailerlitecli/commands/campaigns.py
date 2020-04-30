aea985b3dc03be0ad9325a06bf8d59fa37534027
Alex Orfanos <aorf@pressidium.com>
2020-04-30 07:59:46 +0100

    Fix createABCampaign() - pending testing, currently returns 500

--- mailerlitecli/commands/campaigns.py
+++ mailerlitecli/commands/campaigns.py
@@ -1,3 +1,4 @@
+import mailerlitecli.commands.groups as groups 
 import requests
 import re
 
@@ -5,7 +6,7 @@ def getCampaignStatus(mailerlite_api_token):
     _campaigns_status_all = requests.get('https://api.mailerlite.com/api/v2/campaigns', headers={'X-MailerLite-ApiKey': '{}'.format(mailerlite_api_token)}).json()
     print(_campaigns_status_all)
 
-def createABCampaign(mailerlite_api_token, groups, values, send_type, ab_win_type, split_part):
+def createABCampaign(mailerlite_api_token, groups, values, send_type, ab_win_type, winner_after, split_part):
     headers = {
             "Content-Type": "application/json",
             "X-MailerLite-ApiKey": "{}".format(mailerlite_api_token),
@@ -15,20 +16,19 @@ def createABCampaign(mailerlite_api_token, groups, values, send_type, ab_win_typ
     _winner_after_time_int = _winner_after[0]
     _winner_after_type = _winner_after[1]
 
-    if campaign_type == "ab":
-        data = {
-                'groups': '{}'.format(groups),
-                'type': 'ab',
-                'ab_settings': {
-                    'send_type': '{}'.format(send_type),
-                    'values': '{}'.format(values),
-                    'ab_win_type': '{}'.format(ab_win_type),
-                    'winner_after': '{}'.format(_winner_after),
-                    'winner_after_type': '{}'.format(_winner_after_type),
-                    'split_part': '{}'.format(split_part),
-                    },
-                }
+    data = {
+            "groups": "{}".format(groups),
+            "type": "ab",
+            "ab_settings": {
+                "send_type": "{}".format(send_type),
+                "values": "{}".format(values),
+                "ab_win_type": "{}".format(ab_win_type),
+                "winner_after": "{}".format(_winner_after),
+                "winner_after_type": "{}".format(_winner_after_type),
+                "split_part": "{}".format(split_part),
+                },
+            }
 
-        _create_campaign = requests.post("https://api.mailerlite.com/api/v2/campaigns", headers=headers, json=data)
+    _create_campaign = requests.post("https://api.mailerlite.com/api/v2/campaigns", headers=headers, json=data)
 
-        print(_create_campaign)
+    print(_create_campaign.content)
