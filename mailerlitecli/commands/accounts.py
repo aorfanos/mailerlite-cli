6b8b243330e365e32a23dc0c1775a0b0d2d66cc6
Alex Orfanos <aorf@pressidium.com>
2020-04-30 10:50:30 +0100

    Add support for UNIX timestamp to show

--- mailerlitecli/commands/accounts.py
+++ mailerlitecli/commands/accounts.py
@@ -6,9 +6,14 @@ class Account(object):
     def __init__(self, mailerlite_api_token):
         self.mailerlite_api_token = mailerlite_api_token
 
-    def stats(self):
+    def stats(self, timestamp=""):
         mailerlite_api_token = self.mailerlite_api_token
         response_table = PrettyTable()
         response_table.field_names = ['Key', 'Value']
-        _account_stats = requests.get("https://api.mailerlite.com/api/v2/stats", headers={'X-MailerLite-ApiKey': '{}'.format(mailerlite_api_token)}).json()
+        if timestamp == "":
+            _account_stats = requests.get("https://api.mailerlite.com/api/v2/stats", headers={'X-MailerLite-ApiKey': '{}'.format(mailerlite_api_token)}).json()
+        else:
+            _account_stats = requests.get("https://api.mailerlite.com/api/v2/stats", 
+                    headers={'X-MailerLite-ApiKey': '{}'.format(mailerlite_api_token)},
+                        json={'timestamp': '{}'.format(timestamp)}).json()
         print(_account_stats)
